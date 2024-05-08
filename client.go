86b2e06fc6625a3e09da7bf512f6ae6255dec621
Jasper Bok <hello@jasperbok.nl>
2024-05-08 21:21:23 +0200

    Add the `CompanyCode` field to `Client`

--- client.go
+++ client.go
@@ -4,12 +4,14 @@ import "net/http"
 
 type Client struct {
 	client      *http.Client
+	CompanyCode string
 }
 
 // NewClient creates a new client to work with the Ximmio Waste API.
-func NewClient() *Client {
+func NewClient(companyCode string) *Client {
 	client := &Client{
-		client: http.DefaultClient,
+		client:      http.DefaultClient,
+		CompanyCode: companyCode,
 	}
 
 	return client
