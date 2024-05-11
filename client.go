578c76d6db3ad14ac763b0221de997cdd12bdf52
Jasper Bok <hello@jasperbok.nl>
2024-05-11 10:32:21 +0200

    Add the `GetCalendars` method

--- client.go
+++ client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 const (
@@ -67,4 +68,41 @@ func (c *Client) GetAddress(postCode string, houseNumber int) (Address, error) {
 	}
 
 	return responseStruct.DataList[0], nil
+}
+
+func (c *Client) GetCalendars(start, end time.Time, addressId string) (Calendars, error) {
+	calendars := Calendars{}
+
+	url := fmt.Sprintf("%s/GetCalendar", baseURL)
+
+	requestData := struct {
+		CompanyCode string `json:"companyCode"`
+		StartDate   string `json:"startDate"`
+		EndDate     string `json:"endDate"`
+		AddressID   string `json:"uniqueAddressId"`
+	}{c.CompanyCode, start.Format("2006-01-02"), end.Format("2006-01-02"), addressId}
+
+	data, err := json.Marshal(requestData)
+	if err != nil {
+		return calendars, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return calendars, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return calendars, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return calendars, err
+	}
+
+	err = json.Unmarshal(body, &calendars)
+	return calendars, err
 }
