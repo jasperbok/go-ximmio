ea6926f2395520aacf94f9cdeb1dcf5ca8eda512
Jasper Bok <hello@jasperbok.nl>
2024-11-16 11:09:57 +0100

    Complete the `Address` type
    Also changed the exported field name `ZipCode` to `PostalCode`, as a zip
    code is not a translation for 'postcode', but a completely different
    concept.

--- address.go
+++ address.go
@@ -2,12 +2,20 @@ package ximmio
 
 // Address is a collection of address information as returned by Ximmio's
 // Waste API.
+//
+// The actual API response also has a field `BuildingCategory`. However, at the
+// time of writing this client has only been tested with residential addresses,
+// for which the field always seem to be `null`. So no data type can be defined
+// for it yet.
 type Address struct {
-	UniqueID    string `json:"UniqueId"`
-	Street      string `json:"Street"`
-	HouseNumber string `json:"HouseNumber"`
-	HouseLetter string `json:"HouseLetter"`
-	ZipCode     string `json:"ZipCode"`
-	City        string `json:"City"`
-	Community   string `json:"Community"`
+	ID                    int    `json:"ID"`
+	UniqueID              string `json:"UniqueId"`
+	Street                string `json:"Street"`
+	HouseNumber           string `json:"HouseNumber"`
+	HouseLetter           string `json:"HouseLetter"`
+	HouseNumberIndication string `json:"HouseNumberIndication"`
+	HouseNumberAddition   string `json:"HouseNumberAddition"`
+	PostalCode            string `json:"ZipCode"`
+	City                  string `json:"City"`
+	Community             string `json:"Community"`
 }
