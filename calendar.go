44b45f019ed7f4bdb5a520bca8af0425aafad0ad
Jasper Bok <hello@jasperbok.nl>
2024-11-17 20:23:54 +0100

    Add the `Description` field to `Calendar`

--- calendar.go
+++ calendar.go
@@ -4,6 +4,7 @@ type Calendar struct {
 	PickupDates    []Time `json:"pickupDates"`
 	PickupType     int    `json:"pickupType"`
 	PickupTypeText string `json:"_pickupTypeText"`
+	Description    string `json:"description"`
 }
 
 type Calendars struct {
